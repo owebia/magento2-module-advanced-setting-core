879859863319d61da52f4edc65a2052fd98bf455
Antoine Lemoine <antoine.lemoine@gmail.com>
2016-02-08 11:58:37 +0100

    Use repository for attribut set

--- Model/Wrapper/ProductAttributeSet.php
+++ Model/Wrapper/ProductAttributeSet.php
@@ -9,12 +9,36 @@ class ProductAttributeSet extends SourceWrapper
 {
 
     /**
-     * @return \Magento\Eav\Model\Entity\Attribute\Set
+     * @var \Magento\Eav\Api\AttributeSetRepositoryInterface
+     */
+    protected $attributeSetRespository;
+
+    /**
+     * @param \Magento\Eav\Api\AttributeSetRepositoryInterface $attributeSetRespository
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Magento\Backend\Model\Auth\Session $backendAuthSession
+     * @param \Magento\Quote\Model\Quote\Address\RateRequest $request
+     * @param \Owebia\ShippingCore\Helper\Registry $registry
+     * @param mixed $data
+     */
+    public function __construct(
+        \Magento\Eav\Api\AttributeSetRepositoryInterface $attributeSetRespository,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Backend\Model\Auth\Session $backendAuthSession,
+        \Magento\Quote\Model\Quote\Address\RateRequest $request,
+        \Owebia\ShippingCore\Helper\Registry $registry,
+        $data = null
+    ) {
+        parent::__construct($objectManager, $backendAuthSession, $request, $registry, $data);
+        $this->attributeSetRespository = $attributeSetRespository;
+    }
+
+    /**
+     * @return \Magento\Eav\Api\Data\AttributeSetInterface
      */
     protected function loadSource()
     {
-        return $this->objectManager
-            ->create('Magento\Eav\Model\Entity\Attribute\Set')
-            ->load($this->data['id']);
+        return $this->attributeSetRespository
+            ->get($this->data['id']);
     }
 }
